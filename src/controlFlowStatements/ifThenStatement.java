a5bb63cd505da80009bb59c2a1e5befa5f5a8845
Nagendra Reddy <nagendrareddy29029@gmail.com>
2022-07-09 21:19:14 +0530

    operator practice

---
+++ src/controlFlowStatements/ifThenStatement.java
@@ -0,0 +1,20 @@
+package controlFlowStatements;
+
+public class ifThenStatement {
+    public static void main(String[] args) {
+        boolean isAlien = true;
+        if (isAlien == false) {
+            System.out.println("It is Not Alien");
+            System.out.println("I am not Scared of Aliens");
+
+        }
+
+        int topScore = 90;
+        int secondTopScore = 85;
+        if (topScore > secondTopScore && topScore < 100) {
+            System.out.println("Top Score is Greater than Second top Score and less than 100");
+
+        }
+
+    }
+}
--- src/operatorsOperandsAndExpressions/Operators.java
+++ src/operatorsOperandsAndExpressions/Operators.java
@@ -5,6 +5,15 @@ public class Operators {
         int result = 1 + 2;
         System.out.println(result);
         System.out.println(3+5);
+        int number = 1;
+        number++;
+        System.out.println(number);
+        ++number;
+        System.out.println(number);
+        int sum = 50;
+        sum *= 2;
+        System.out.println(sum);
+
 
     }
 }
